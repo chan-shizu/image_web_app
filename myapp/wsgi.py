b84eb4aaf97f8f27a3420ecd00f7e293391c0447
chan-shizu <bubbler018@gmail.com>
2020-12-29 01:25:25 +0900

    change wsgi

--- myapp/wsgi.py
+++ myapp/wsgi.py
@@ -1,8 +1,8 @@
-#import os
+import os
 from django.core.wsgi import get_wsgi_application
 #from whitenoise.django import DjangoWhiteNoise
 
-#os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myapp.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myapp.settings')
 
 application = get_wsgi_application()
 
