03cf679189d4514bc94120a613247d879b5da769
chan-shizu <bubbler018@gmail.com>
2020-12-28 15:29:07 +0900

    change settings

--- myapp/settings.py
+++ myapp/settings.py
@@ -138,7 +138,7 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 #STATIC_ROOT = 'staticfiles'
 #STATIC_ROOT = 'static'
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 try:
     from .local_settings import *
