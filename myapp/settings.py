1f74169699950b18ee3de5e80347f2c06391809e
chan-shizu <bubbler018@gmail.com>
2020-12-29 19:20:43 +0900

    change setting

--- myapp/settings.py
+++ myapp/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'app/media')
 MEDIA_URL = '/media/'
 
 # herokuにデプロイするために追加
