3152e5448d7baf34171e9e0fae8c31febdb99966
chan-shizu <bubbler018@gmail.com>
2020-12-30 00:48:57 +0900

    change settings

--- myapp/settings.py
+++ myapp/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
 #STATIC_URL = '/staticfiles/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
