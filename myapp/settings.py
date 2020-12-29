fd6efb50292bdd1503c917854c5b1c59c411d933
chan-shizu <bubbler018@gmail.com>
2020-12-29 11:48:59 +0900

    add favicon

--- myapp/settings.py
+++ myapp/settings.py
@@ -128,8 +128,8 @@ MEDIA_URL = '/media/'
 # herokuにデプロイするために追加
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 #STATICFILES_DIRS = (
 #   os.path.join(BASE_DIR, 'static'),
