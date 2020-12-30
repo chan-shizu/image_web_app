345d0ade4875b50d45c4e9184498bf05670a105a
chan-shizu <bubbler018@gmail.com>
2020-12-30 22:26:44 +0900

    change models.py

--- app1/models.py
+++ app1/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 import os.path
 import datetime
+from myapp.settings import BASE_DIR
 
 def update_filename(instance, filename):
-    path = "document/"
+    path = BASE_DIR + 'media/document/"
     now = datetime.datetime.now()
     format = now.strftime('%Y%m%d_%H%M%S') + '.jpg'
     return os.path.join(path, format)
@@ -12,4 +13,4 @@ class Document(models.Model):
     description = models.CharField(max_length=255, blank=True)
     photo = models.ImageField(upload_to=update_filename, default='defo')
     uploaded_at = models.DateTimeField(auto_now_add=True)
-    output = models.ImageField(default = 'output/output.jpg')
+    output = models.ImageField(default = BASE_DIR + 'media/output/output.jpg')
